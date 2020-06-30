b0d684d6979a5226fd94d43207f6081e1af09420
macyw6 <macyw@lafayette.edu>
2020-06-30 14:52:22 -0400

    yas

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/sunflower2.jpg') {
+      myImage.setAttribute ('src','images/sunflower.jpg');
+    } else {
+      myImage.setAttribute ('src','images/sunflower2.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name',myName);
+    myHeading.textContent = 'Mozilla is cool, ' +myName;
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
